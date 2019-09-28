e74abee923d5a468b1077b7395ad198951ad8837
Emma <aryyil2@gmail.com>
2019-09-28 19:29:58 +0100

    add reset button

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
+function deleteGrid() {
+    gridItems.forEach(gridItem => container.removeChild(gridItem));
+}
+
+function promptForReset() {
+    let input = prompt("How large should the grid be? Enter a number between 1-80:");
+
+    if (input == null) {
+        msg.textContent = "Reset was cancelled.";
+    } else if (input > 0 && input < 81) {
+        deleteGrid();
+        createGrid(input);
+        msg.textContent = `New ${input}x${input} grid created.`;
+    } else {
+        msg.textContent = "Invalid input, grid was not reset.";
+    }
+}
+
 function colorCell(e) {
     this.classList.add("colored");
 }
 
-const container = document.getElementById("container1");
-const gridItem = document.createElement("div");
+function createGrid(gridSize) {
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
-gridItem.classList.add("grid", "grid-item");
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        container.appendChild(gridItem.cloneNode(false));
+    }
 
-let rowCount = 16;
-let colCount = 16;
-let totalCells = rowCount * colCount;
-
-container.style.gridTemplateRows = `repeat(${rowCount}, 1fr)`;
-container.style.gridTemplateColumns = `repeat(${colCount}, 1fr)`;
-
-for (let i = 0; i < totalCells; i++) {
-    container.appendChild(gridItem.cloneNode(false));
+    gridItems = document.querySelectorAll(".grid-item");
+    gridItems.forEach(gridItem => gridItem.addEventListener("mouseenter", colorCell));
 }
 
-const gridItems = document.querySelectorAll(".grid-item");
-gridItems.forEach(gridItem => gridItem.addEventListener("mouseenter", colorCell));
+const container = document.getElementById("container1");
+const gridItem = document.createElement("div");
+const msg = document.getElementById("msg");
+
+gridItem.classList.add("grid", "grid-item");
+let gridItems;
+
+createGrid(16);
